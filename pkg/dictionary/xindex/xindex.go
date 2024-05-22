62efa3488a1c4eb019b5cf882f63fdc2733e259c
Arman <arman.saqarchi@gmail.com>
2024-05-22 17:21:32 +0330

    feat(xindex): add function templates for inverted index

---
+++ pkg/dictionary/xindex/xindex.go
@@ -0,0 +1,43 @@
+package dictionary
+
+
+import (
+	"rimor/pkg/dictionary/record"
+)
+
+
+type XIndexer interface{
+	GetRecord(string) record.Recorder
+	GetRecords() []record.Recorder
+	GetDocumentsNum() int
+	Sort()
+}
+
+type Xindex struct {
+	Records []record.Recorder
+	DocNum int
+	Sorted bool
+}
+
+
+func (x *Xindex) GetRecord(string) record.Recorder {
+	return nil
+}
+
+func (x *Xindex) GetRecords(string) []record.Recorder {
+	return []record.Recorder{}
+}
+
+func (x *Xindex) GetDocumentsNum() int {
+	return 0
+}
+
+func (x *Xindex) Sort() int {
+	return 0
+}
+
+
+
+
+
+
