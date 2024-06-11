56c7a66c2268736790d838b29dfadb9732edb94c
Arman <arman.saqarchi@gmail.com>
2024-06-11 13:19:59 +0330

    feat(Document): add document Unmarshaller func

--- pkg/preprocessing/preprocessing.go
+++ pkg/preprocessing/preprocessing.go
@@ -1,13 +1,54 @@
 package preprocessing
 
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"strconv"
+)
+
 type DocumentCollection struct {
 	DocList []Document
 }
 
 type Document struct {
-	Id         int64  `json:"id"`
-	DocUrl     string `json:"url"`
-	DocContent string `json:"text"`
+	ID       int64 
+	Title    string 
+	Content  string 
+	Date     string     // change the type to time.time if necessary
+	Url      string 
+	Tags	[]string 
+	Category string 
+}
+
+type Documents struct {
+	DocumentList []Document
+}
+
+func (ds *Documents) UnmarshalJSON(d []byte) error {
+
+	var m map[string]Document
+	if err := json.Unmarshal(d, &m); err != nil {
+		log.Printf("failed to parse raw string into map, err : %s", err.Error())
+	}
+	for k, v := range m {
+		id, err := strconv.ParseInt(k, 10, 64)
+		if err != nil {
+			return fmt.Errorf("failed to convert string id into integer id, err : %s", err.Error())
+		}
+		ds.DocumentList = append(ds.DocumentList, Document{
+			ID: id,
+			Title: v.Title,
+			Content: v.Content,
+			Date: v.Date,
+			Url: v.Url,
+			Tags: v.Tags,
+			Category: v.Category,
+
+		})
+	}
+
+	return nil
 }
 
 type PreprocessingStep interface {
@@ -52,20 +93,17 @@ func (preprocessor *Preprocessor) applyAllPreprocessingSteps(document Document) 
 	return processed
 }
 
-
 type TkDocument struct {
-	Id         int64 
-	DocUrl     string
+	Id                 int64
+	DocUrl             string
 	TokenzedDocContent []string
 }
-
 
 type TkDocumentCollection struct {
 	DocList []TkDocument
 }
 
 type Tokenizer struct {
-
 }
 
 func (tokenizer *Tokenizer) Tokenize(documentCollection DocumentCollection) TkDocumentCollection {
