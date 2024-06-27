efa997dc034293c0219e1cec0db82b8906825e0e
farbod <farbod.bjary82@gmail.com>
2024-06-27 14:20:40 +0330

    fix(dictionary): Fixes problem for one word queries

--- pkg/engine/dictionary/xindex/xindex.go
+++ pkg/engine/dictionary/xindex/xindex.go
@@ -6,8 +6,7 @@ import (
 	"rimor/pkg/utils/errors"
 )
 
-
-type XIndexer interface{
+type XIndexer interface {
 	GetRecord(string) record.Recorder
 	GetRecords() []record.Recorder
 	GetDocumentsNum() int
@@ -15,12 +14,11 @@ type XIndexer interface{
 }
 
 type Xindex struct {
-	DocLengths []int64
-	Records []record.Recorder
-	DocNum int64
+	DocLengths  []int64
+	Records     []record.Recorder
+	DocNum      int64
 	ChampionNum int
 }
-
 
 func (x *Xindex) GetRecord(string) record.Recorder {
 	return nil
@@ -38,38 +36,22 @@ func (x *Xindex) Sort() int {
 	return 0
 }
 
-func (x *Xindex) BinarySearchRecord(t string) (record.Recorder, error){
+func (x *Xindex) BinarySearchRecord(t string) (record.Recorder, error) {
 	if x.Records == nil {
 		return nil, fmt.Errorf("failed to get the target record, Records list are empty")
 	}
 	s, e := 0, len(x.Records)-1
 
-
 	for s <= e {
-		if s == e{
-			if t == x.Records[s].GetTerm(){
-				return x.Records[s], nil
-			} else {
-				return nil, errors.NewRecordNotFound(nil)
-			}
+		mid := (s + e) / 2
+		if t == x.Records[mid].GetTerm() {
+			return x.Records[mid], nil
+		} else if t < x.Records[mid].GetTerm() {
+			e = mid - 1
 		} else {
-			mid := (s + e) / 2
-			if t >= x.Records[mid].GetTerm(){
-				s = mid+1
-			} else {
-				e = mid-1
-			}
+			s = mid + 1
 		}
 	}
 
-	return nil, fmt.Errorf("something went wrong while doing binary search")
-} 
-
-
-
-
-
-
-
-
-
+	return nil, errors.NewRecordNotFound(nil)
+}
